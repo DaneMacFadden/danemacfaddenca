f11fc7441af6d6190571e88d65387f36e6c10594
Dane MacFadden <danem1000@hotmail.com>
2026-07-26 19:50:26 -0600

    Updated styling of tab bar

--- src/components/Tabbar.tsx
+++ src/components/Tabbar.tsx
@@ -4,9 +4,9 @@ function Tabbar() {
   return (
     <>
         <div className="btn-group col-md-4 p-2">
-            <button type="button" className="btn btn-outline-dark">Home</button>
-            <button type="button" className="btn btn-outline-dark">About Me</button>
-            <button type="button" className="btn btn-outline-dark">My Projects</button>
+            <button type="button" className="btn btn-outline-light">Home</button>
+            <button type="button" className="btn btn-outline-light">About Me</button>
+            <button type="button" className="btn btn-outline-light">My Projects</button>
         </div>   
     </>
   )
