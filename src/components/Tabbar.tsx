58c99a0e6fab09a49bc73c5dbf66d515c9c55351
Dane MacFadden <danem1000@hotmail.com>
2026-07-26 19:40:12 -0600

    Added a basic tab bar

--- src/components/Tabbar.tsx
+++ src/components/Tabbar.tsx
@@ -3,7 +3,11 @@ function Tabbar() {
 
   return (
     <>
-        <p>Hi</p>
+        <div className="btn-group col-md-4 p-2">
+            <button type="button" className="btn btn-outline-dark">Home</button>
+            <button type="button" className="btn btn-outline-dark">About Me</button>
+            <button type="button" className="btn btn-outline-dark">My Projects</button>
+        </div>   
     </>
   )
 }
