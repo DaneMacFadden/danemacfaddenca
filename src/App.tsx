092b4831e8f79068fc9e0c985791e8dfa7ed5a6a
Dane MacFadden <danem1000@hotmail.com>
2026-07-26 19:25:39 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Tabbar from './components/Tabbar'
+function App() {
+
+  return (
+    <>
+      <Tabbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Tabbar.tsx
@@ -0,0 +1,11 @@
+
+function Tabbar() {
+
+  return (
+    <>
+        <p>Hi</p>
+    </>
+  )
+}
+
+export default Tabbar
