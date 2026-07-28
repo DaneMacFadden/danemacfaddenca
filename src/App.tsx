e422fde8478181987a3bc27fe0b949866f346406
Dane MacFadden <danem1000@hotmail.com>
2026-07-28 15:23:50 -0600

    Added card component, centered 3 cards. Consider adding card grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Tabbar from './components/Tabbar'
+import Card from './components/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -7,6 +8,12 @@ function App() {
   return (
     <>
       <Tabbar />
+      <div className="d-flex gap-3 justify-content-center">
+          <Card img="https://cdn.pixabay.com/photo/2022/12/23/01/25/steering-7673519_1280.jpg" text="This is some sample text" />
+          <Card img="https://cdn.pixabay.com/photo/2022/12/23/01/25/steering-7673519_1280.jpg" text="This is some sample text" />
+          <Card img="https://cdn.pixabay.com/photo/2022/12/23/01/25/steering-7673519_1280.jpg" text="This is some sample text" />
+
+      </div>
     </>
   )
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface CardProps {
+    img: string       // The location of the image
+    text: string      // The text the card will contain
+}
+
+function Card({ img, text }: CardProps) {
+    return (
+        <div className="card" style={{ width: "18rem" }}>
+            <img src={img} className="card-img-top" />
+            <div className="card-body">
+                <p className="card-text">{text}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
