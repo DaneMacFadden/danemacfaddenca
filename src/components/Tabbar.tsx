afb460d3725cec638fee76e9ec628009bc50c368
Dane MacFadden <danem1000@hotmail.com>
2026-08-05 17:27:44 -0600

    Added socials icons to Tabbar component

--- src/components/Tabbar.tsx
+++ src/components/Tabbar.tsx
@@ -1,15 +1,18 @@
 import { NavLink } from 'react-router-dom'
+import { FiGithub } from "react-icons/fi";
+import { CiLinkedin } from "react-icons/ci";
+import { TiSocialLinkedin } from "react-icons/ti";
 
 function Tabbar() {
   return (
     <>
       <nav className="w-full my-5 outline outline-gray-500 rounded p-5 px-10">
-        <div className="md:w-auto">
-          <ul className="font-medium flex justify-start flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+        <div className="w-full">
+          <ul className="font-medium flex flex-row justify-start space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
             <li>
-              <NavLink 
-                to="/" 
-                className={({ isActive }) => 
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
                   `inline-block transform transition-all duration-300 hover:scale-150 cursor-pointer ${isActive ? 'text-white' : 'text-heading focus:text-white'}`
                 }
               >
@@ -17,9 +20,9 @@ function Tabbar() {
               </NavLink>
             </li>
             <li>
-              <NavLink 
-                to="/About" 
-                className={({ isActive }) => 
+              <NavLink
+                to="/About"
+                className={({ isActive }) =>
                   `inline-block transform transition-all duration-300 hover:scale-150 cursor-pointer ${isActive ? 'text-white' : 'text-heading focus:text-white'}`
                 }
               >
@@ -27,16 +30,25 @@ function Tabbar() {
               </NavLink>
             </li>
             <li>
-              <NavLink 
+              <NavLink
                 to="/Projects"
-                className={({ isActive }) => 
+                className={({ isActive }) =>
                   `inline-block transform transition-all duration-300 hover:scale-150 cursor-pointer ${isActive ? 'text-white' : 'text-heading focus:text-white'}`
                 }
               >
                 Projects
               </NavLink>
             </li>
+            {/* Icons container now aligns perfectly to the right */}
+            <li className="ml-auto">
+          <ul className="flex flex-row items-center space-x-4 justify-end">
+              <li><FiGithub className="cursor-pointer hover:text-white" /></li>
+              <li><CiLinkedin className="cursor-pointer hover:text-white" /></li>
+              <li><TiSocialLinkedin className="cursor-pointer hover:text-white" /></li>
+            </ul>
+            </li>
           </ul>
+          
         </div>
       </nav>
     </>
