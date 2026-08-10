8d57603b0c52b057cff79f76acc49d68811403df
Dane MacFadden <danem1000@hotmail.com>
2026-08-09 18:26:14 -0600

    Added link to my paper in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <>
       <Textbox>This section contains a bit about me and my hobbies outside of computer science so you can get to know me! I'm an avid amateur astronomer. I even published an astronomy paper while I was in university.
-        <Button link="https://google.com">Click here to see my paper!</Button>
+        <Button link="https://www.jdso.org/volume20/number4/MacFadden_463_474.pdf">Click here to see my paper!</Button>
       </Textbox>
     </>
   )
