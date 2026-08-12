4b68b017923165f08b32ac35c9ab53ad61643ad9
Dane MacFadden <danem1000@hotmail.com>
2026-08-11 18:54:14 -0600

    Changed Textbox paddinga

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -4,7 +4,7 @@ interface TextboxProps {
 
 function Textbox({ children }: TextboxProps) {
     return (
-        <div className="rounded-md bg-limie p-5">
+        <div className="rounded-md bg-limie p-10">
             <p className="text-limietxt mx-10 text-sm md:text-base lg:text-lg my-5">{ children }</p>
         </div>
     )
