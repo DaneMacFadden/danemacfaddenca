45390276de70a74557272321bc53364ec1b3d104
Dane MacFadden <danem1000@hotmail.com>
2026-08-13 18:19:33 -0600

    Updated ImgCard and About components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import Textbox from "./Textbox"
 import Button from "./Button"
+import ImgCard from "./ImgCard"
 
 // To Do: Implement expanding image cards
 
@@ -11,7 +12,11 @@ function About() {
         astronomy class. The study covers double star system observations that we took using Skynet's PROMPT telescopes. Skynet is a website that gives students and researchers access to remote robotic telescopes around the world.
         Just enter what celestial object you want to look at, and Skynet schedules a telescope to take pictures for you. One of these telescopes is located just outside Saskatoon, and is maintained by Dr. Daryl Janzen of the Univeristy of Saskatchewan,
         who also taught most of my astronomy classes throughout university. While I was enrolled in one of his classes, he let me help out fixing a problem the telescope was having. Since I love both computers and astronomy, that was quite an exciting experience!
-        <br /><h1>IMAGE CARDS WILL GO HERE</h1><br />
+        <br />
+        <div className="flex place-content-center mx-5">
+          <ImgCard imgloc="">Test</ImgCard><ImgCard imgloc="">Test</ImgCard>
+        </div>
+        <br />
         During the astronomy class, Dr. Janzen told the class about a program called ERIRA. It's a study abroad program, hosted by Dr. Daniel Reichart from the University of North Carolina at Chapel Hill, who also hosts Skynet. ERIRA takes
         place at the Greenbank Observatory in West Virginia. There, I got to use and learn all about radio telescopes, which I likely would never have seen otherwise. See the expandable cards below to learn more!
         <br /><h1>IMAGE CARDS WILL GO HERE</h1><br />
--- src/components/ImgCard.tsx
+++ src/components/ImgCard.tsx
@@ -1,13 +1,17 @@
+import { motion } from "motion/react"
+
 interface ImgCardProps {
-    location: string
+    imgloc: string
     children: React.ReactNode
 
 }
 
-function ImgCard() {
+function ImgCard({imgloc, children}: ImgCardProps) {
     return (
         <>
-        <div></div>
+            <motion.div className="bg-darklimie text-limietxt-active rounded-md w-50 h-50 m-5 p-5">
+                <div className="pt-35">{ children }</div>
+            </motion.div>
         </>
     )
 }
