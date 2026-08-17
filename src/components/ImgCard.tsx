1627dd290f7127b3e023519b1209f45327e8bd43
Dane MacFadden <danem1000@hotmail.com>
2026-08-16 21:18:43 -0600

    Added settling animation to ImgCard component

--- src/components/ImgCard.tsx
+++ src/components/ImgCard.tsx
@@ -2,6 +2,7 @@ import { motion } from "motion/react"
 
 interface ImgCardProps {
     imgloc: string
+    delay: number
     children: React.ReactNode
 
 }
@@ -9,7 +10,7 @@ interface ImgCardProps {
 function ImgCard({imgloc, children}: ImgCardProps) {
     return (
         <>
-            <motion.div className="bg-darklimie text-limietxt-active rounded-md w-50 h-50 m-5 p-5">
+            <motion.div className="bg-darklimie text-limietxt-active rounded-md w-50 h-50 m-5 p-5" initial={{y: -50, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{ ease: "easeIn", duration: 1}}>
                 <div className="pt-35">{ children }</div>
             </motion.div>
         </>
