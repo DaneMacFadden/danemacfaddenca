3108d62c1c15c055ec34db55049bc5381921eaf5
Dane MacFadden <danem1000@hotmail.com>
2026-08-20 18:09:35 -0600

    Updated imgcard to be more customizable

--- src/components/ImgCard.tsx
+++ src/components/ImgCard.tsx
@@ -2,16 +2,16 @@ import { motion } from "motion/react"
 
 interface ImgCardProps {
     imgloc: string
-    delay: number
+    textProps: string
     children: React.ReactNode
-
 }
 
-function ImgCard({imgloc, children}: ImgCardProps) {
+function ImgCard({imgloc, textProps, children}: ImgCardProps) {
     return (
         <>
-            <motion.div className="bg-darklimie text-limietxt-active rounded-md w-50 h-50 m-5 p-5" initial={{y: -50, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{ ease: "easeIn", duration: 1}}>
-                <div className="pt-35">{ children }</div>
+            <motion.div className="bg-darklimie text-limietxt-active rounded-md w-50 h-55 m-5" initial={{y: -28, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{ ease: "easeOut", duration: 1.5}}>
+                <div className="p-2"><img className="rounded-md" src={ imgloc }></img></div>
+                <div className={ textProps }>{ children }</div>
             </motion.div>
         </>
     )
