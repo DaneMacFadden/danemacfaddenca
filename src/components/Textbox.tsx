e5d1072a19dc18620c841f83a2b9604700318336
Dane MacFadden <danem1000@hotmail.com>
2026-08-20 18:11:25 -0600

    Removed animations from Textbox

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -1,14 +1,12 @@
-import { motion } from "motion/react"
-
 interface TextboxProps {
     children: React.ReactNode
 }
 
 function Textbox({ children }: TextboxProps) {
     return (
-        <motion.div className="rounded-md bg-limie p-10 mx-10" initial={{ y: -15, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ ease: "easeOut", duration: 1}}>
+        <div className="rounded-md bg-limie p-10 mx-10" >
             <p className="text-limietxt mx-10 text-sm md:text-base lg:text-lg my-5">{children}</p>
-        </motion.div>
+        </div>
     )
 }
 
